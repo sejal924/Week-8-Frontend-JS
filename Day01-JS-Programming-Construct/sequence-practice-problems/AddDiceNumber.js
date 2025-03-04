29a1fe29adf494004823bd78fb04743e2b467408
sejal924 <sejalchouhan24@gmail.com>
2025-03-04 15:27:10 +0530

    [Sejal]Add.adding two random dice number

---
+++ Day01-JS-Programming-Construct/sequence-practice-problems/AddDiceNumber.js
@@ -0,0 +1,5 @@
+// 3. Add two Random Dice Numbers and Print the Result
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
+let sumOfDice = dice1 + dice2;
+console.log("Dice 1:", dice1, ",Dice 2:", dice2, ",Sum:", sumOfDice);
