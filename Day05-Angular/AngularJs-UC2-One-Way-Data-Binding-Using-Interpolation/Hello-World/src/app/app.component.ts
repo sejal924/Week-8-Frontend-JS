f6b6489009080a2a5fe089e15c487ae7961345f3
sejal924 <sejalchouhan24@gmail.com>
2025-03-09 11:12:22 +0530

    [Sejal]ADD.implemented one-way data binding using interpolation

---
+++ Day05-Angular/AngularJs-UC2-One-Way-Data-Binding-Using-Interpolation/Hello-World/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Hello-World';
+
+  ngOnInit(): void{
+    this.title = "Hello from BridgeLabz.";
+  }
+}
